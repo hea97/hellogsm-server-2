905b68dd775c5673a0847bf48ed5e45f1fd17c1e
허태희 <s24054@gsm.hs.kr>
2025-03-24 16:04:16 +0900

    🔧 appspec.yml 및 after_install.sh 수정: cleanup.sh 스크립트를 BeforeInstall 훅에 추가하고 Git 관련 파일 삭제 메시지 주석 처리

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -9,7 +9,7 @@ TAR_FILE=my-django-app.tar.gz
 # Git 관련 파일 삭제
 echo "> Git 관련 파일 삭제 중..." >> /home/ec2-user/deploy.log
 rm -f /home/ec2-user/aws-study/.git/COMMIT_EDITMSG || { echo "COMMIT_EDITMSG 삭제 실패!"; exit 1; }
-rm -rf /home/ec2-user/aws-study/.git/* || { echo "기타 Git 파일 삭제 실패!"; exit 1; }
+# rm -rf /home/ec2-user/aws-study/.git/* || { echo "기타 Git 파일 삭제 실패!"; exit 1; }
 
 echo "> 현재 실행 중인 Docker 컨테이너 ID 확인" >> /home/ec2-user/deploy.log
 CURRENT_CONTAINER_ID=$(docker ps -q -f name=$CONTAINER_NAME)
